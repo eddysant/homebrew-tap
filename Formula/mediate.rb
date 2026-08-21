43c07be45cb99c73b8437ca407b04185deeb67da
Eddy Sant <eddysant@gmail.com>
2026-08-21 10:41:19 -0400

    mediate 0.8.7

--- Formula/mediate.rb
+++ Formula/mediate.rb
@@ -1,8 +1,8 @@
 class Mediate < Formula
   desc "Standardize photos and videos with strict validation before trashing originals"
   homepage "https://github.com/eddysant/mediate"
-  url "https://github.com/eddysant/mediate/archive/refs/tags/v0.8.6.tar.gz"
-  sha256 "caa5f68848f281558cfbdd857c4f81a90fca809be5b33a5454d2e2cdabfb2152"
+  url "https://github.com/eddysant/mediate/archive/refs/tags/v0.8.7.tar.gz"
+  sha256 "12ac3b430f2484223d996a31352d84e8081c0ebce5b625f90b951b172316acb0"
   license "MIT"
 
   depends_on "exiftool"
